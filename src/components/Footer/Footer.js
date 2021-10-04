8e5f416d1f5e6f868a57bb5115a44cd180a2cc62
shahinurrahman22004 <shahinur02345@gmail.com>
2021-10-04 14:54:50 +0600

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,34 +3,40 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="container footer-extra">
-                <div>
-                    <h4>Educate</h4>
-                    <p>Providing Life Changing Experiences Through Education. Class That Fit Your Busy Life. Closer to Home</p>
-                    <p>01310365997</p>
-                    <p>Sat-Thu 8:00-16:00</p>
-                </div>
-                <div>
-                    <h4>Latest News</h4>
-                    <p>Graduate Admissions</p>
-                    <small>by shahinur rahman</small>
-                    <br />
-                    <p>Continuing Education</p>
-                    <small>by shahinur rahman</small>
-                    <br />
-                    <p>Current Students</p>
-                    <small>by shahinur rahman</small>
-                </div>
-                <div>
-                    <h4>Useful Links</h4>
-                    <p>Home</p>
-                    <p>About Us</p>
-                    <p>Services</p>
-                    <p>Contact</p>
+        <div>
+            <div className="footer">
+                <div className="container footer-extra">
+                    <div>
+                        <h4>Educate</h4>
+                        <p>Providing Life Changing Experiences Through Education. Class That Fit Your Busy Life. Closer to Home</p>
+                        <p>01310365997</p>
+                        <p>Sat-Thu 8:00-16:00</p>
+                    </div>
+                    <div>
+                        <h4>Latest News</h4>
+                        <p>Graduate Admissions</p>
+                        <small>by shahinur rahman</small>
+                        <br />
+                        <p>Continuing Education</p>
+                        <small>by shahinur rahman</small>
+                        <br />
+                        <p>Current Students</p>
+                        <small>by shahinur rahman</small>
+                    </div>
+                    <div>
+                        <h4>Useful Links</h4>
+                        <p>Home</p>
+                        <p>About Us</p>
+                        <p>Services</p>
+                        <p>Contact</p>
+                    </div>
                 </div>
             </div>
+            <div style={{textAlign: 'center'}}>
+                <p style={{color: 'gray'}}>Copyright © Secondary Kindergarten Theme by Stylemix Themes</p>
+            </div>
         </div>
+        
     );
 };
 
